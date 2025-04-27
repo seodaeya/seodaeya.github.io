82f411f5dba8f6701a215c981a37c44d97c7aaf0
서대석 <seodaeya@gmail.com>
2025-04-28 02:15:19 +0900

    basePath 제거거

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // 정적 사이트로 내보내기 설정
-  basePath: '',     // 기본 경로 (필요 시 수정)
+  // basePath: '',     // 기본 경로 (필요 시 수정)
 };
 
 export default nextConfig;
