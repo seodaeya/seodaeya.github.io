58aca414c7a41e57e6d44127bd51d8d535bf2486
NaRD <seodaeya@gmail.com>
2026-06-02 00:20:03 +0900

    feat: integrate Google AdSense script globally in head

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import '@/styles/globals.css';
 import Layout from '@/components/Layout';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        {/* Google AdSense Integration */}
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9646087317436086"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
