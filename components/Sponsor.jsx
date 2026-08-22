ad4a5e02d27b55f582f6bceb4460018320a56895
NaRD <seodaeya@gmail.com>
2026-08-22 10:35:50 +0900

    feat(monetization): 후원 컴포넌트에 GitHub Sponsors(수수료 0% 글로벌 후원) 버튼 추가 연동

--- components/Sponsor.jsx
+++ components/Sponsor.jsx
@@ -16,6 +16,7 @@ export default function Sponsor({ title = "따뜻한 커피 한 잔으로 응원
         </p>
 
         <div className={styles.sponsorActions}>
+          {/* KakaoPay QR Button */}
           <button 
             type="button"
             className={styles.kakaoPayButton}
@@ -29,8 +30,27 @@ export default function Sponsor({ title = "따뜻한 커피 한 잔으로 응원
             >
               <path d="M12 3c-5.523 0-10 3.582-10 8 0 2.85 1.875 5.347 4.717 6.726l-1.196 4.385c-.105.385.312.695.65.475l5.244-3.486c.193.013.388.02.585.02 5.523 0 10-3.582 10-8s-4.477-8-10-8z"/>
             </svg>
-            카카오페이로 커피 한 잔 후원하기
+            카카오페이 후원
           </button>
+
+          {/* GitHub Sponsors Button */}
+          <a 
+            href="https://github.com/sponsors/seodaeya" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className={styles.githubSponsorButton}
+            aria-label="GitHub Sponsors로 후원하기"
+          >
+            <svg 
+              className={styles.githubIcon} 
+              viewBox="0 0 24 24" 
+              fill="currentColor"
+            >
+              <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z"/>
+            </svg>
+            <span className={styles.sponsorHeart}>💖</span>
+            GitHub Sponsors
+          </a>
         </div>
       </section>
 
